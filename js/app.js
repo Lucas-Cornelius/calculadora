461619ea7bfaf62c3171080656778ac9525e9ae1
Lucas Cornelius <lucasv.cornelius@gmail.com>
2024-04-07 20:28:05 -0300

    funcoes prontas

---
+++ js/app.js
@@ -0,0 +1,31 @@
+function sendValue(x) {
+    let valor = document.getElementById('display'); 
+
+    if(x == '=') {
+        let porcentagem = valor.innerHTML.includes('%');
+
+        if(porcentagem) {
+            let valorArray = valor.innerHTML.split('%');
+            calculo = eval(valorArray[0])*eval(valorArray[1])*0.01;
+            if(isNaN(calculo)) {
+                alert('Houve um erro, tente novamente!');
+                valor.innerHTML = '';
+                return
+            }
+            valor.innerHTML = calculo;
+        }
+
+        valor.innerHTML = eval(valor.innerHTML);
+        return;
+    } else if(x == 'clear') {
+        valor.innerHTML = '';
+        return;
+    } else if(x == 'del') {
+        let novoValor = ((valor.innerHTML).split('')).slice(0,-1);
+        valor.innerHTML = novoValor.join('');
+        return;
+    } else {
+        document.getElementById('display').innerHTML += x;
+    }
+
+}
